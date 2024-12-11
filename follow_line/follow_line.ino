97ae6aa4e76c299ac6b81d86a62a29285f94a4f0
Daniel Guillén <d.guillen.2022@alumnos.urjc.es>
2024-12-11 20:56:39 +0100

    Added movement

--- follow_line/follow_line.ino
+++ follow_line/follow_line.ino
@@ -1,8 +1,8 @@
 #include "thread.h"
 #include "FastLED.h"
+
 #define PIN_RBGLED 4
 #define NUM_LEDS 1
-CRGB leds[NUM_LEDS];
 
 #define PIN_ITR20001_LEFT   A2
 #define PIN_ITR20001_MIDDLE A1
@@ -19,13 +19,69 @@ CRGB leds[NUM_LEDS];
 #define PIN_Motor_BIN_1 8
 #define PIN_Motor_PWMB 6
 
-ThreadController controller = ThreadController();
+#define KP 0.24 // Last 0.12 0.18 -- Perfect: 0.24
+#define KD 0.04 // Last 0.04 -- Perfect: 0.04
 
+#define VEL_BASE 60
+#define MAX_VEL 60
+#define MIN_VEL 30
+
+CRGB leds[NUM_LEDS];
+ThreadController controller = ThreadController();
 UltraSound* ultraSound;
+
+int ACK_COUNTER = 1;
+
+int vel = 60;
+int last_error = 0;
 
 uint32_t Color(uint8_t r, uint8_t g, uint8_t b)
 {
   return (((uint32_t)r << 16) | ((uint32_t)g << 8) | b);
+}
+
+void ackWiFi() {
+  String recvBuff;
+  while(1) {
+    if (Serial.available()) {
+      char c = Serial.read();
+      recvBuff += c;
+
+      if (c == '}')  {
+        if (recvBuff == "{ 'wifi': 1 }") {
+          FastLED.showColor(Color(0, 255, 0));
+
+          // Send an ACK to the ESP32
+          Serial.print("{ 'ack': " + String(ACK_COUNTER) + " }");
+          ACK_COUNTER++;
+          break;
+        }
+        recvBuff = "";
+      }
+    }
+  }
+}
+
+void ackMQTT() {
+  String recvBuff;
+  while(1) {
+    if (Serial.available()) {
+      char c = Serial.read();
+      recvBuff += c;
+
+      if (c == '}')  {
+        if (recvBuff == "{ 'mqtt': 1 }") {
+          FastLED.showColor(Color(0, 255, 0));
+
+          // Send an ACK to the ESP32
+          Serial.print("{ 'ack': " + String(ACK_COUNTER) + " }");
+          ACK_COUNTER++;
+          break;
+        }
+        recvBuff = "";
+      }
+    }
+  }
 }
 
 void setup() {
@@ -34,8 +90,12 @@ void setup() {
 
   FastLED.addLeds<NEOPIXEL, PIN_RBGLED>(leds, NUM_LEDS);
   FastLED.setBrightness(20);
-  int r=255,g=255,b=255;
-  FastLED.showColor(Color(r, g, b));
+
+  FastLED.showColor(Color(255, 0, 0));
+  ackWiFi();
+  FastLED.showColor(Color(255, 0, 0));
+  ackMQTT();
+  FastLED.showColor(Color(0, 255, 0));
 
   ultraSound = new UltraSound(TRIG_PIN, ECHO_PIN);
   ultraSound->setInterval(100);
@@ -50,15 +110,13 @@ void setup() {
 
   pinMode(PIN_Motor_STBY, OUTPUT);
   digitalWrite(PIN_Motor_STBY, HIGH);
-  analogWrite(PIN_Motor_PWMB, 0);
-  analogWrite(PIN_Motor_PWMA, 0);
+  digitalWrite(PIN_Motor_AIN_1, HIGH);
+  digitalWrite(PIN_Motor_BIN_1, HIGH);
+  
 }
 
 void loop() {
   controller.run();
-
-  digitalWrite(PIN_Motor_AIN_1, HIGH);
-  digitalWrite(PIN_Motor_BIN_1, LOW);
   // put your main code here, to run repeatedly:
   int left = analogRead(PIN_ITR20001_LEFT);
   int mid = analogRead(PIN_ITR20001_MIDDLE);
@@ -70,7 +128,7 @@ void loop() {
   Serial.println(mid);
   Serial.print("Right: ");
   Serial.println(right);
-  delay(1000);
+  //delay(1000);
 
   float dist = ultraSound->getDistance();
   Serial.print("Ultrasound: ");
@@ -78,4 +136,24 @@ void loop() {
 
   int r=255,g=0,b=0;
   FastLED.showColor(Color(r, g, b));
+
+  if (left > 200 || mid > 200 || right > 200) {
+    int base_speed = 80; // 80 100 140 in no close turns
+    int error = right-left;
+    int correction = KP * error + KD * (error - last_error);
+
+    int left_speed = base_speed - correction;
+    int right_speed = base_speed + correction;
+
+    left_speed = constrain(left_speed, 0, 130);
+    right_speed = constrain(right_speed, 0, 130);
+
+    Serial.print(left_speed);
+    Serial.print("\n");
+    Serial.print(right_speed);
+    Serial.print("\n");
+
+    analogWrite(PIN_Motor_PWMA, left_speed);
+    analogWrite(PIN_Motor_PWMB, right_speed);
+  }
 }
