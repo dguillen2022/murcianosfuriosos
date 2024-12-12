3d992419b4515b665da40af4fd15101c435f0bae
avalen2022 <a.valencia.2022@alumnos.urjc.es>
2024-12-12 15:11:12 +0100

    Best laps

--- follow_line/follow_line.ino
+++ follow_line/follow_line.ino
@@ -20,7 +20,7 @@
 #define PIN_Motor_PWMB 6
 
 #define KP 0.24 // Last 0.12 0.18 -- Perfect: 0.24 in class 0.01 another 0.0045
-#define KD 0.04 // Last 0.04 -- Perfect: 0.04 in class 0.08 another 0.053
+#define KD 0.012 // Last 0.04 -- Perfect: 0.04 in class 0.08 another 0.053
 
 #define VEL_BASE 60
 #define MAX_VEL 60
@@ -129,7 +129,8 @@ void loop() {
     analogWrite(PIN_Motor_PWMB, 0);
   } else {
     if (left > 200 || mid > 200 || right > 200) {
-      int base_speed = 100; // 80 100 140 in no close turns
+      FastLED.showColor(Color(0, 255, 0));
+      int base_speed = 250;
       int error = right-left;
       int correction = KP * error + KD * (error - last_error);
 
@@ -144,6 +145,7 @@ void loop() {
       analogWrite(PIN_Motor_PWMA, right_speed);
       analogWrite(PIN_Motor_PWMB, left_speed);
     } else if (left < 200 && mid < 200 && right < 200) {
+      FastLED.showColor(Color(255, 0, 0));
       if (last_error < 0) { // derecha
         analogWrite(PIN_Motor_PWMA, 200);
         analogWrite(PIN_Motor_PWMB, 0);
